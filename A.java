249d22ebe0dfbdd188d2d05ae6030d70ed21be85
NaveenkumarMIT <naveen.gcebargur@gmail.com>
2023-09-19 18:33:42 +0530

    tested

---
+++ A.java
@@ -0,0 +1,22 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("my ld!");
+		System.out.println("Hello World!");
+		System.out.println("H!");
+		System.out.println("H!");
+		System.out.println("H!");
+		System.out.println("H!");
+		System.out.println("H!");
+		System.out.println("H!");
+		System.out.println("H!");
+		System.out.println("Hello World!");
+		System.out.println("end");
+	}
+}
